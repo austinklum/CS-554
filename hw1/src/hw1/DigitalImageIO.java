f70f3e8f4a55c484e332bbf460915298e8ea5741
Austin Klum <klumaustin@gmail.com>
2020-09-30 02:25:45 -0500

    Fixed factory and usage

--- hw1/src/hw1/DigitalImageIO.java
+++ hw1/src/hw1/DigitalImageIO.java
@@ -7,21 +7,26 @@ import java.util.Scanner;
 public class DigitalImageIO {
     public enum ImageType { INDEXED, PACKED, LINEAR_ARRAY, MULTIDIM_ARRAY };
     private static String magicNumber;
-    private int width;
-    private int height;
     private int maxColorValue;
     
     
     public static DigitalImage read( File file, ImageType type ) throws IOException, IllegalFileFormatException {
     	Scanner scan = new Scanner(file);
     	magicNumber = scan.next();
+    	int width = Integer.parseInt(scan.next());
+    	int height = Integer.parseInt(scan.next());
     	
+    	DigitalImage image = new ImageFactory().GetImage(type, width, height);
+    	scanFileToImage(scan, file, image);
     	
-    	
-    	return null;
+    	return image;
     }
     
-    public static void write( File file, DigitalImage image ) throws IOException {
+    private static void scanFileToImage(Scanner scan, File file, DigitalImage image) {
+ 
+	}
+
+	public static void write( File file, DigitalImage image ) throws IOException {
         // TO-DO : write this method
     }
 }
--- hw1/src/hw1/ImageFactory.java
+++ hw1/src/hw1/ImageFactory.java
@@ -5,21 +5,21 @@ import hw1.DigitalImageIO.ImageType;
 
 public class ImageFactory 
 {
-	public DigitalImage GetImage(ImageType type, int width, int height)
+	ImageFactory
+	public DigitalImage GetImage(ImageType type, int width, int height) throws IllegalFileFormatException
 	{
 		switch (type)
 		{
 			case INDEXED:
 				return new IndexedDigitalImage(width, height, null);
 			case PACKED: 
-				return new PackedPixelImage();
+				return new PackedPixelImage(width, height, 8);
 			case LINEAR_ARRAY:
-				return new LinearArrayDigitalImage();
+				return new LinearArrayDigitalImage(width, height, 8);
 			case MULTIDIM_ARRAY :
-				return new ArrayDigitalImage();
+				return new ArrayDigitalImage(width, height, 8);
 			default:
 				throw new IllegalFileFormatException("Type of " + type + " was not found");
-			
 		}
 	}
 }

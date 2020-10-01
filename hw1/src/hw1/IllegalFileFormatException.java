2ef19d06b6a8b40f3fa7e90fad0e2ba415c7e2e0
Austin Klum <klumaustin@gmail.com>
2020-09-30 21:40:04 -0500

    Finished first attempt at implementing IndexedDigitalImage

--- hw1/src/hw1/IllegalFileFormatException.java
+++ hw1/src/hw1/IllegalFileFormatException.java
@@ -8,7 +8,7 @@ public class IllegalFileFormatException extends IOException {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	public IllegalFileFormatException() {
+	protected IllegalFileFormatException() {
 		super();
 	}
 	public IllegalFileFormatException(String loggingMsg) {
--- hw1/src/hw1/IndexedDigitalImage.java
+++ hw1/src/hw1/IndexedDigitalImage.java
@@ -3,45 +3,52 @@ package hw1;
 import java.awt.Color;
 
 public class IndexedDigitalImage extends AbstractDigitalImage implements DigitalImage {
+	private static final int BANDS = 1;
+	private static final int MAX_PALETTE_SIZE = 256;
+
+	private byte[] raster;
 	private Color[] palette;
-	private static final int BPP = 8;
-
-	@Override
-	public int[] getPixel(int x, int y) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void setPixel(int x, int y, int[] pixel) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public int getSample(int x, int y, int band) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public void setSample(int x, int y, int band, int sample) {
-		// TODO Auto-generated method stub
-
-	}
 	
 	public IndexedDigitalImage(int width, int height)
 	{
-		super(width, height, BPP);
-		palette = new Color[256];
+		super(width, height, BANDS);
+		raster = new byte[width * height * BANDS];
+		palette = new Color[MAX_PALETTE_SIZE];
 	}
 	
 	public IndexedDigitalImage(int width, int height, Color[] palette)
 	{
-		super(width, height, BPP);
+		super(width, height, BANDS);
+		raster = new byte[width * height * BANDS];
 		this.palette = palette;
 	}
 	
+	@Override
+	public int[] getPixel(int x, int y)
+	{
+        int[] result = new int[bands];
+        System.arraycopy(raster, bands * (x + y * width), result, 0, bands);
+        return result;
+	}
+
+	@Override
+	public void setPixel(int x, int y, int[] pixel)
+	{
+		System.arraycopy(pixel, 0, raster, bands * (x + y * width), bands);
+	}
+
+	@Override
+	public int getSample(int x, int y, int band)
+	{
+		return raster[bands * (x + y * width) + band];
+	}
+
+	@Override
+	public void setSample(int x, int y, int band, int sample)
+	{
+		 raster[bands * (x + y * width) + band] = (byte) sample;
+	}
+
 	public void setPaletteColor(int paletteIndex, Color color)
 	{
 		this.palette[paletteIndex] = color;

5e9a2ed4237f7e13ad9279a124c07cb3c3d818ad
Austin Klum <klumaustin@gmail.com>
2020-10-12 18:02:16 -0500

    Added a NullOp

--- hw2/src/hw2/HorizontalFlipOp.java
+++ hw2/src/hw2/HorizontalFlipOp.java
@@ -6,50 +6,42 @@ import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.awt.image.BufferedImageOp;
 import java.awt.image.ColorModel;
+import java.awt.image.WritableRaster;
 
-public class HorizontalFlipOp implements BufferedImageOp, pixeljelly.ops.PluggableImageOp
+import pixeljelly.scanners.Location;
+import pixeljelly.scanners.RasterScanner;
+
+public class HorizontalFlipOp extends NullOp implements BufferedImageOp, pixeljelly.ops.PluggableImageOp
 {
+	public HorizontalFlipOp() 
+	{
+		
+	}
+	
 
 	@Override
-	public BufferedImage createCompatibleDestImage(BufferedImage arg0, ColorModel arg1) {
-		// TODO Auto-generated method stub
-		return null;
+	public BufferedImage filter(BufferedImage src, BufferedImage dest) 
+	{
+		if (dest == null)
+		{
+			dest = createCompatibleDestImage(src, src.getColorModel());
+		}
+		
+		WritableRaster srcRaster = src.getRaster();
+		WritableRaster destRaster = dest.getRaster();
+		
+		for (Location pt : new RasterScanner(src, true))
+		{
+			int sample = srcRaster.getSample(pt.col,pt.row, pt.band);
+			destRaster.setSample(pt.col, pt.row, pt.band, sample);
+		}
+		return dest;
 	}
 
 	@Override
-	public BufferedImage filter(BufferedImage arg0, BufferedImage arg1) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Rectangle2D getBounds2D(BufferedImage arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Point2D getPoint2D(Point2D arg0, Point2D arg1) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public RenderingHints getRenderingHints() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String getAuthorName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public BufferedImageOp getDefault(BufferedImage arg0) {
-		// TODO Auto-generated method stub
-		return null;
+	public BufferedImageOp getDefault(BufferedImage src)
+	{
+		return new HorizontalFlipOp();
 	}
 	
 

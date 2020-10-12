9378ed7b1151ed6611c0a093e450675bf116cdc8
Austin Klum <klumaustin@gmail.com>
2020-10-11 21:54:41 -0500

    added pixeljelly jar and started first op

---
+++ hw2/src/hw2/HorizontalFlipOp.java
@@ -0,0 +1,56 @@
+package hw2;
+
+import java.awt.RenderingHints;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.awt.image.BufferedImageOp;
+import java.awt.image.ColorModel;
+
+public class HorizontalFlipOp implements BufferedImageOp, pixeljelly.ops.PluggableImageOp
+{
+
+	@Override
+	public BufferedImage createCompatibleDestImage(BufferedImage arg0, ColorModel arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BufferedImage filter(BufferedImage arg0, BufferedImage arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Rectangle2D getBounds2D(BufferedImage arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Point2D getPoint2D(Point2D arg0, Point2D arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public RenderingHints getRenderingHints() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getAuthorName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BufferedImageOp getDefault(BufferedImage arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}

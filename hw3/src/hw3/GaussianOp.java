74a56620a37357922550182fd6703e20582c5ed3
Austin Klum <klumaustin@gmail.com>
2020-11-03 23:31:30 -0600

    GaussianOp looks good

--- hw3/src/hw3/GaussianOp.java
+++ hw3/src/hw3/GaussianOp.java
@@ -5,11 +5,16 @@ import java.awt.image.BufferedImage;
 import java.awt.image.BufferedImageOp;
 import java.awt.image.ColorModel;
 import java.awt.image.WritableRaster;
+import java.lang.reflect.InvocationTargetException;
 
+import pixeljelly.ops.ConvolutionOp;
 import pixeljelly.scanners.Location;
 import pixeljelly.scanners.RasterScanner;
+import pixeljelly.utilities.Kernel2D;
+import pixeljelly.utilities.ReflectivePadder;
+import pixeljelly.utilities.SeperableKernel;
 
-public class GaussianOp extends NullOp implements  BufferedImageOp, pixeljelly.ops.PluggableImageOp
+public class GaussianOp extends ConvolutionOp implements  BufferedImageOp, pixeljelly.ops.PluggableImageOp
 {
 	
 	private double alpha;
@@ -22,40 +27,35 @@ public class GaussianOp extends NullOp implements  BufferedImageOp, pixeljelly.o
 	
 	public GaussianOp(double alpha, double sigma) 
 	{
+		super(createGuassianKernal(alpha,sigma), true, ReflectivePadder.getInstance());
 		this.setAlpha(alpha);
 		this.setSigma(sigma);
-	}
-
-	@Override
-	public BufferedImage filter(BufferedImage src, BufferedImage dest) 
-	{
-		if (dest == null)
+		if (isEdgeCase())
 		{
-			dest = createCompatibleDestImage(src, src.getColorModel());
-		}
-		
-		WritableRaster srcRaster = src.getRaster();
-		WritableRaster destRaster = dest.getRaster();
-		
-		RasterScanner rs = new RasterScanner(src, true);
-		for(Location pt : rs)
-		{
-			int sample = srcRaster.getSample(pt.col,pt.row, pt.band);
-			destRaster.setSample(pt.row, pt.col, pt.band, sample);	
-		}
-
-		return dest;
+			Kernel2D newKernel = new SeperableKernel(new float[] { 1.0f }, new float[] { 1.0f });
+			this.setKernel(newKernel);
+		}		
 	}
 
-	public BufferedImage createCompatibleDestImage(BufferedImage src, ColorModel destCM) 
+	private boolean isEdgeCase()
 	{
-		return new BufferedImage(
-				destCM,
-				destCM.createCompatibleWritableRaster(src.getWidth(), src.getHeight()),
-				destCM.isAlphaPremultiplied(), 
-				null);
+		return (this.getKernel().getWidth() == 0 || this.getKernel().getHeight() == 0) ||
+			   (this.getKernel().getWidth() == 1 && this.getKernel().getHeight() == 1);
 	}
-
+	
+	private static Kernel2D createGuassianKernal(double alpha, double sigma)
+	{
+		int w = (int) Math.ceil(alpha * sigma);
+		  float[] result = new float[w * 2 + 1];
+	        for (int n = 0; n <= w; n++) {
+	            double value = Math.exp(-(n * n) / (2.0f * sigma * sigma)) / (Math.sqrt(6.283185307179586) * sigma);
+	            result[result.length / 2 + n] = (float) value;
+	            result[result.length / 2 - n] = (float) value;
+	        }
+	        Kernel2D kernel = new SeperableKernel(result, result);
+	        return kernel;
+	}
+	
 	public double getAlpha()
 	{
 		return alpha;
@@ -74,6 +74,27 @@ public class GaussianOp extends NullOp implements  BufferedImageOp, pixeljelly.o
 	public void setSigma(double sigma) 
 	{
 		this.sigma = sigma;
+	}
+
+	@Override
+	public String getAuthorName()
+	{
+		return "Austin Klum";
+	}
+
+	@Override
+	public BufferedImageOp getDefault(BufferedImage arg0) 
+	{
+		try 
+		{
+			return this.getClass().getConstructor().newInstance();
+		} 
+		catch (Exception e)
+		{
+			System.out.println(e.getMessage());
+			System.exit(-1);
+		}
+		throw new IllegalArgumentException();
 	}
 
 }

ed971ce1b2e81d624c06213748047a85c3818394
Austin Klum <klumaustin@gmail.com>
2020-11-07 22:32:38 -0600

    I'm able to create a histogram now!

---
+++ hw3/src/hw3/ColorHistogram.java
@@ -0,0 +1,16 @@
+package hw3;
+
+import java.awt.image.BufferedImage;
+
+public class ColorHistogram 
+{
+	private enum ColorModel
+	{
+		RGB,
+		HSB
+	}
+	public ColorHistogram(BufferedImage src, ColorModel colorModel, int xn, int yn, int zn)
+	{
+		
+	}
+}
--- hw3/src/hw3/ImageDatabase.java
+++ hw3/src/hw3/ImageDatabase.java
@@ -1,27 +1,24 @@
 package hw3;
 
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
 import java.io.IOException;
+import java.net.URL;
 
+import javax.imageio.ImageIO;
 import javax.naming.directory.InvalidAttributesException;
+
+import pixeljelly.scanners.Location;
+import pixeljelly.scanners.RasterScanner;
 
 public class ImageDatabase 
 {
-
 	public static void main(String[] args)
 	{
 		try {
-			if(args[0].equals("create"))
-			{
-				//parseCreateArgs(args);
-			}
-			else if (args[0].equals("query"))
-			{
-				//parseQueryArgs(args)
-			}
-			else
-			{
-				throw new InvalidAttributesException();
-			}
+			ImageDatabase imgDB = new ImageDatabase(args);
+			imgDB.run();
 		}
 		catch (Exception ex)
 		{
@@ -34,5 +31,180 @@ public class ImageDatabase
 		}
 	}
 	
+	private enum ColorModel
+	{
+		RGB,
+		HSB
+	}
 	
+	private enum CommandType
+	{
+		CREATE,
+		QUERY
+	}
+	private CommandType commandType;
+	private int xn;
+	private int yn;
+	private int zn;
+	private String urlFile;
+	private String dbFile;
+	private ColorModel colorModel;
+	
+	private String queryUrl;
+	private String responseFile;
+	private int kNumberOfImages;
+	
+	private static final int MAX_RGB = 256;
+
+	public ImageDatabase(String[] args) throws InvalidAttributesException 
+	{
+		setDefaults();
+		commandType = CommandType.valueOf(args[0].toUpperCase());
+		if(isCreate())
+		{
+			parseCreateArgs(args);
+		}
+		else if (isQuery())
+		{
+			parseQueryArgs(args);
+		}
+		else
+		{
+			throw new InvalidAttributesException();
+		}
+	}
+	
+	private void setDefaults()
+	{
+		commandType = null;
+		
+		xn = 2;//-1;
+		yn = 2;//-1;
+		zn = 2;//-1;
+		urlFile = null;
+		dbFile = null;
+		colorModel = null;
+		
+		queryUrl = null;
+		responseFile = null;
+		kNumberOfImages = -1;
+	}
+	
+	private boolean isCreate()
+	{
+		return commandType == CommandType.CREATE;
+	}
+	
+	private boolean isQuery()
+	{
+		return commandType == CommandType.QUERY;
+	}
+	
+	private void parseCreateArgs(String[] args) throws InvalidAttributesException
+	{
+		xn = (int) Math.pow(Integer.parseInt(args[1]), 2);
+		yn = (int) Math.pow(Integer.parseInt(args[2]), 2);
+		zn = (int) Math.pow(Integer.parseInt(args[3]), 2);
+		if((xn + yn + zn) > 1024)
+		{
+			throw new InvalidAttributesException("Sum of xn yn zn is greater than 10");
+		}
+		urlFile = args[4];
+		dbFile = args[5];
+		colorModel = ColorModel.valueOf(args[6]);
+	}
+	
+	private void parseQueryArgs(String[] args)
+	{
+		queryUrl = args[1];
+		dbFile = args[2];
+		responseFile = args[3];
+		kNumberOfImages = Integer.parseInt(args[4]);
+	}
+	
+	private void run()
+	{
+		try
+		{
+			if(isCreate())
+			{
+				runCreate();
+			}
+			else if (isQuery())
+			{
+				runQuery();
+			}
+		}
+		catch (Exception ex)
+		{
+			System.out.println(ex);
+			System.out.println(ex.getStackTrace());
+			System.out.println("Exiting program...");
+			System.exit(-1);
+		}
+	}
+	
+	private void runCreate()
+	{
+		// loadFile
+		// LoopOverFile
+		// constructHistogram(image);
+		// writeDB(histograms);
+	}
+	
+	private void runQuery() throws IOException
+	{
+		 List<Histogram> db = loadDB();
+		BufferedImage queryImage = ImageIO.read(new URL(queryUrl));
+		double[] histogram = constructHistogram(queryImage);
+		 HashMap<Histogram, Double> histogramSimilarity = computeSimilarity(histogram, db);
+		// HashMap<Histogram, Double> topImages = filterTopKImages();
+		// createResponseFile(topImages);
+	}
+	
+	private double[] constructHistogram(BufferedImage img)
+	{
+		double[] histogram = new double[xn*yn*zn];
+		
+		WritableRaster imgRaster = img.getRaster();
+		for(Location pt : new RasterScanner(img, false))
+		{
+			int[] rgb = new int[3];
+			imgRaster.getPixel(pt.col, pt.row, rgb);
+			Color color = new Color(rgb[0], rgb[1], rgb[2]);
+			int index = getIndex(color);
+			histogram[index]++;
+		}
+		//printHistogram(histogram);
+		return histogram;
+	}
+	
+	private int getIndex(Color color)
+	{
+		int red = color.getRed();
+		int green = color.getGreen();
+		int blue = color.getBlue();
+		
+		int redPrime = getPrime(red, xn, MAX_RGB);
+		int greenPrime = getPrime(green, yn, MAX_RGB);
+		int bluePrime = getPrime(blue, zn, MAX_RGB);
+		
+		int index = (redPrime * xn * zn) + (greenPrime * yn) + (bluePrime);
+		
+		return index;
+	}
+	
+	private int getPrime(int value, int numberOfBins, int maxValue)
+	{
+		int prime = Math.floorDiv(value * numberOfBins, maxValue);
+		return prime;
+	}
+	
+	private void printHistogram(double[] histogram)
+	{
+		for(int i = 0; i < histogram.length; i++)
+		{
+			System.out.print(i + ":" + histogram[i] + ", ");
+		}
+	}
 }

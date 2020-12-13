38a9e0e3a217b2ae970956836d80a919d2465ffe
Austin Klum <klumaustin@gmail.com>
2020-12-12 18:31:20 -0600

    added header to DCT encode

--- hw5/src/hw5/DCTEncoder.java
+++ hw5/src/hw5/DCTEncoder.java
@@ -1,13 +1,28 @@
 package hw5;
 
 import java.awt.image.BufferedImage;
+import java.io.DataOutput;
+
 import hw5.Compressor.Model;
 
-public class DCTEncoder implements Encoder
-{
+public class DCTEncoder implements Encoder {
 	@Override
-	public void encode(BufferedImage image, Model model, int[] N, String output) 
+	public void encode(BufferedImage image, Model model, int[] N, String output) throws Exception 
 	{
 		
+	}
+
+	@Override
+	public void writeHeader(BufferedImage image, DataOutput out) throws Exception 
+	{
+		out.writeUTF(getMagicWord());
+		out.writeShort(image.getWidth());
+		out.writeShort(image.getHeight());
+	}
+
+	@Override
+	public String getMagicWord() 
+	{
+		return "DFT";
 	}
 }
--- hw5/src/hw5/Encoder.java
+++ hw5/src/hw5/Encoder.java
@@ -1,9 +1,12 @@
 package hw5;
 
 import java.awt.image.BufferedImage;
+import java.io.DataOutput;
 import hw5.Compressor.Model;
 
 public interface Encoder 
 {
-	public void encode(BufferedImage image, Model model, int[] N, String output);
+	public void writeHeader(BufferedImage image, DataOutput out) throws Exception;
+	public void encode(BufferedImage image, Model model, int[] N, String output) throws Exception;;
+	public String getMagicWord();
 }

4145c57427056b6d0c29edf516f08aa357aba499
Austin Klum <klumaustin@gmail.com>
2020-12-13 17:51:20 -0600

    Delta Encoder header

--- hw5/src/hw5/DeltaEncoder.java
+++ hw5/src/hw5/DeltaEncoder.java
@@ -1,10 +1,10 @@
 package hw5;
 
 import java.awt.image.BufferedImage;
-import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.OutputStream;
+
+import javax.imageio.stream.MemoryCacheImageOutputStream;
 
 import hw5.Compressor.Model;
 
@@ -13,9 +13,18 @@ public class DeltaEncoder implements Encoder
 	@Override
 	public void encode(BufferedImage image, Model model, int[] N, File output) throws Exception
 	{
-		DataOutputStream out = new DataOutputStream(new FileOutputStream(output));
+		MemoryCacheImageOutputStream out = new MemoryCacheImageOutputStream(new FileOutputStream(output));
 		writeHeader(image, out);
-		
+		out.writeInt(N[0]);
+		out.writeInt(N[1]);
+		out.writeInt(N[2]);
+		for (int band = 0; band < image.getRaster().getNumBands(); band++)
+		{
+			for (Location pt : new RasterScanner(image, false))
+			{
+				
+			}
+		}
 		
 		out.close();
 	}

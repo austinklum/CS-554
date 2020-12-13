b996c071c96eabab35104fa539cddd585d4beea2
Austin Klum <klumaustin@gmail.com>
2020-12-13 12:46:22 -0600

    added readHeader();

--- hw5/src/hw5/Decoder.java
+++ hw5/src/hw5/Decoder.java
@@ -4,17 +4,17 @@ import java.awt.image.BufferedImage;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 
 public interface Decoder 
 {
 	public String getMagicWord();
-	public void decode(File input, File output);
-	public default boolean canDecode(File output)
+	public void decode(File input, File output) throws Exception;
+	public default boolean canDecode(DataInputStream in)
 	{
 		String magicWord = null;
 		try
 		{
-			DataInputStream in = new DataInputStream(new FileInputStream(output));
 			magicWord = in.readUTF();
 			in.close();
 		}
@@ -23,5 +23,13 @@ public interface Decoder
 			return false;
 		}
 		return magicWord != null && magicWord.equals(getMagicWord());
+	}
+	
+	public default BufferedImage readHeader(DataInputStream in) throws Exception
+	{
+		short width = in.readShort();
+		short height = in.readShort();
+		int imageType = in.readInt();
+		return new BufferedImage(width, height, imageType);
 	}
 }
--- hw5/src/hw5/RLEDecoder.java
+++ hw5/src/hw5/RLEDecoder.java
@@ -1,15 +1,37 @@
 package hw5;
 
+import java.awt.image.BufferedImage;
+import java.io.DataInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class RLEDecoder implements Decoder
 {
 	@Override
-	public void decode(File input, File output) 
+	public void decode(File input, File output) throws Exception 
 	{
-		if (!canDecode(output))
+		DataInputStream in = new DataInputStream(new FileInputStream(output));
+		if (!canDecode(in)) return;
+		
+		BufferedImage image = readHeader(in);
+		for (int band = 0; band < image.getSampleModel().getNumBands(); band++)
 		{
-			return;
+			for (int bit = 0; bit < image.getSampleModel().getSampleSize(band); bit++)
+			{
+				for (int y = 0; y < image.getHeight(); y++)
+				{
+					boolean isWhite = true;
+					int pixelsEncoded = 0;
+					while (pixelsEncoded < image.getWidth())
+					{
+						int length = getRun(image, pixelsEncoded, y, band, bit, isWhite);
+						isWhite = !isWhite;
+						writeRun(length, out);
+						pixelsEncoded += length;
+					}
+				}
+			}
 		}
 
 	}

8a5d856cc92a9e91757324e1c0eb9eca032cf305
Austin Klum <klumaustin@gmail.com>
2020-12-13 12:05:38 -0600

    default canDecode();

--- hw5/src/hw5/Decoder.java
+++ hw5/src/hw5/Decoder.java
@@ -1,10 +1,27 @@
 package hw5;
 
 import java.awt.image.BufferedImage;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 
 public interface Decoder 
 {
 	public String getMagicWord();
 	public void decode(BufferedImage image, String output);
-	public void canDecode()
+	public default boolean canDecode(String output)
+	{
+		String magicWord = null;
+		try
+		{
+			DataInputStream in = new DataInputStream(new FileInputStream(new File(output)));
+			magicWord = in.readUTF();
+			in.close();
+		}
+		catch (Exception e)
+		{
+			return false;
+		}
+		return magicWord != null && magicWord.equals(getMagicWord());
+	}
 }

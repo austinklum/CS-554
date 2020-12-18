98a13565c2e79af59eb581b221f0abb6ab862b6f
Austin Klum <klumaustin@gmail.com>
2020-12-18 13:02:38 -0600

    processing args error message

--- exam/src/exam/SeamCarver.java
+++ exam/src/exam/SeamCarver.java
@@ -1,50 +1,58 @@
 package exam;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.LinkedList;
 import java.util.List;
+
+import javax.imageio.ImageIO;
 
 public class SeamCarver
 {
 	private enum Mode { ERASE, SIZE }
 	
 	private Mode mode;
-	private String input;
+	private BufferedImage input;
 	private String output;
 	
 	private List<Parameter> parameters;
 	
-	private class Parameter
-	{
-		int x;
-		int y;
-		int w;
-		int h;
-		public Parameter(int x, int y, int w, int h)
-		{
-			this.x = x;
-			this.y = y;
-			this.w = w;
-			this.h = h;
-		}
-		public Parameter(int w, int h)
-		{
-			this(-1,-1,w,h);
-		}
-	}
+
 	
 	public static void main(String args[])
 	{
 		SeamCarver carver = new SeamCarver(args);
-		//carver.run();
+		carver.run();
 	}
 	
 	public SeamCarver(String[] args)
 	{
-		processArgs(args);
+		try
+		{
+			processArgs(args);
+		}
+		catch (Exception e)
+		{
+			System.out.println("Error Processing Arguments!");
+			System.out.println(e.getMessage());
+			System.out.println(e.getStackTrace());
+			System.exit(-1);
+		}
 	}
 	
+	private void run()
+	{
+		double[][] energy = createEnergyMap();
+	}
+
+	private double[][] createEnergyMap()
+	{
+		return null;
+	}
 	
-	private void processArgs(String[] args)
+	private void processArgs(String[] args) throws Exception
 	{
 		int i = 0;
 		setInput(args[i++]);
@@ -85,12 +93,14 @@ public class SeamCarver
 		this.mode = mode;
 	}
 
-	public String getInput() {
+	public BufferedImage getInput() {
 		return input;
 	}
 
-	public void setInput(String input) {
-		this.input = input;
+	public void setInput(String input) throws Exception
+	{
+		BufferedImage img = ImageIO.read(new URL(input));
+		this.input = img;
 	}
 
 	public String getOutput() {
@@ -99,5 +109,24 @@ public class SeamCarver
 
 	public void setOutput(String output) {
 		this.output = output;
+	}
+	
+	private class Parameter
+	{
+		int x;
+		int y;
+		int w;
+		int h;
+		public Parameter(int x, int y, int w, int h)
+		{
+			this.x = x;
+			this.y = y;
+			this.w = w;
+			this.h = h;
+		}
+		public Parameter(int w, int h)
+		{
+			this(-1,-1,w,h);
+		}
 	}
 }

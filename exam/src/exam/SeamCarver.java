ad1f2aa1e9465191c4fc408410639d19d9610bee
Austin Klum <klumaustin@gmail.com>
2020-12-18 19:25:40 -0600

    EnergyMapping();

--- exam/src/exam/SeamCarver.java
+++ exam/src/exam/SeamCarver.java
@@ -1,8 +1,8 @@
 package exam;
 
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.LinkedList;
 import java.util.List;
@@ -14,12 +14,10 @@ public class SeamCarver
 	private enum Mode { ERASE, SIZE }
 	
 	private Mode mode;
-	private BufferedImage input;
+	private BufferedImage image;
 	private String output;
 	
 	private List<Parameter> parameters;
-	
-
 	
 	public static void main(String args[])
 	{
@@ -45,17 +43,188 @@ public class SeamCarver
 	private void run()
 	{
 		double[][] energy = createEnergyMap();
+		//outputEnergyMap(energy);
+		if (getMode() == Mode.SIZE)
+		{
+			Parameter param = parameters.get(0);
+			sizeImage(param.w, param.h);
+		}
+
 	}
 
+	private void sizeImage(int width, int height)
+	{
+		int horizontalSeams = Math.abs(image.getHeight() - height);
+		int verticalSeams =  Math.abs(image.getWidth() - width);
+		
+		boolean growHorizontal = image.getHeight() - height < 0;
+		boolean growVertical = image.getWidth() - width < 0;
+		
+		while ((horizontalSeams + verticalSeams) > 0)
+		{
+			
+		}
+		
+	}
+	
+	private void outputEnergyMap(double[][] energyTable)
+	{
+		 BufferedImage energy_image = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_RGB);
+
+        //find the max value in the energy table
+        double maxValue = energyTable[0][0];
+        for(int i = 0; i < energyTable.length; i++)
+            for(int j = 0; j < energyTable[0].length;j++)
+                maxValue = Math.max(maxValue, energyTable[i][j]);
+
+        // loop over each pixel
+        for(int i = 0; i < energy_image.getWidth(); i++)
+        {
+            for(int j = 0; j < energy_image.getHeight(); j++)
+            {
+                // calculate the rgb value (scaled for grayscale)
+                int gray = (int) ((energyTable[i][j]/maxValue)*256);
+                int rgb = (gray << 16) + (gray << 8) + gray;
+                energy_image.setRGB(i,j,rgb);
+            }
+        }
+        //save the energy table image
+        try {
+            File output_file = new File("energyTable.jpg");
+            ImageIO.write(energy_image, "jpg", output_file);
+        } catch (IOException e)
+        {
+            System.out.println("Cannot create file for energy table.");
+        }
+    }
+	
 	private double[][] createEnergyMap()
 	{
-		return null;
+		double[][] energy = new double[image.getWidth()][image.getHeight()];
+		
+		for (int y = 0; y < image.getHeight(); y++) 
+		{
+            for (int x = 0; x < image.getWidth(); x++)
+            {
+                energy[x][y] = getEnergyValue(x, y);
+            }
+        }
+		
+		return energy;
+	}
+
+	private double getEnergyValue(int x, int y)
+	{
+		double xEnergy = getXEnergy(x, y);
+		double yEnergy = getYEnergy(x, y);
+
+		return xEnergy + yEnergy;
+	}
+	
+	private double getYEnergy(int x, int y)
+	{
+		int upPixel = getUpPixel(x, y);
+		int downPixel = getDownPixel(x, y);
+		
+		double energy = getEnergy(upPixel, downPixel);
+		
+		return energy;
+	}
+	
+	private double getXEnergy(int x, int y)
+	{
+		int leftPixel = getLeftPixel(x, y);
+		int rightPixel = getRightPixel(x, y);
+		
+		double energy = getEnergy(leftPixel, rightPixel);
+		
+		return energy;
+	}
+
+	private int getUpPixel(int x, int y) 
+	{
+		if (!isTopEdge(y))
+		{
+			y--;
+		} 
+		return image.getRGB(x, y);
+	}
+	
+	private int getDownPixel(int x, int y) 
+	{
+		if (!isBottomEdge(y))
+		{
+			y++;
+		} 
+		return image.getRGB(x, y);
+	}
+	
+	private int getLeftPixel(int x, int y) 
+	{
+		if (!isLeftEdge(x))
+		{
+			x--;
+		} 
+		return image.getRGB(x, y);
+	}
+	
+	private int getRightPixel(int x, int y) 
+	{
+		if (!isRightEdge(x))
+		{
+			x++;
+		} 
+		return image.getRGB(x, y);
+	}
+
+	private boolean isLeftEdge(int x)
+	{
+		return x == 0;
+	}
+	
+	private boolean isRightEdge(int x)
+	{
+		return (x + 1) == image.getWidth();
+	}
+	
+	private boolean isTopEdge(int y)
+	{
+		return y == 0;
+	}
+	
+	private boolean isBottomEdge(int y)
+	{
+		return (y + 1) == image.getHeight();
+	}
+	
+	private double getEnergy(int pixel, int otherPixel)
+	{
+		double redDiff = getRedDifference(pixel, otherPixel);
+		double greenDiff = getGreenDifference(pixel, otherPixel);
+		double blueDiff = getBlueDifference(pixel, otherPixel);
+		
+		return redDiff*redDiff + greenDiff*greenDiff + blueDiff*blueDiff;
+	}
+	
+	private double getBlueDifference(int pixel, int otherPixel) 
+	{
+		return (pixel) & 0xff - (otherPixel) & 0xff;
+	}
+
+	private double getGreenDifference(int pixel, int otherPixel) 
+	{
+		return ((pixel >> 8) & 0xff) - ((otherPixel >> 8) & 0x0000ff00);
+	}
+
+	private double getRedDifference(int pixel, int otherPixel) 
+	{
+		return ((pixel >> 16) & 0xff) - ((otherPixel >> 16) & 0xff) ;
 	}
 	
 	private void processArgs(String[] args) throws Exception
 	{
 		int i = 0;
-		setInput(args[i++]);
+		setImage(args[i++]);
 		setOutput(args[i++]);
 		setMode(Mode.valueOf(args[i++].replace("-", "").toUpperCase()));
 		setParameters(i, args);
@@ -93,14 +262,14 @@ public class SeamCarver
 		this.mode = mode;
 	}
 
-	public BufferedImage getInput() {
-		return input;
+	public BufferedImage getImage() {
+		return image;
 	}
 
-	public void setInput(String input) throws Exception
+	public void setImage(String input) throws Exception
 	{
 		BufferedImage img = ImageIO.read(new URL(input));
-		this.input = img;
+		this.image = img;
 	}
 
 	public String getOutput() {
